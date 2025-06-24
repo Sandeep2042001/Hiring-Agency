5b90779dd8aae1b8155eb90bffe77bfad4b6679c
Sandeep Kumawat <96002657+Sandeep2042001@users.noreply.github.com>
2025-06-24 08:58:41 +0530

    Add .next/static/chunks/src_app_contact_page_tsx_a993527f._.js 1750735718206

---
+++ .next/static/chunks/src_app_contact_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_9314cd7f._.js",
+  "static/chunks/src_da55b85f._.js"
+],
+    source: "dynamic"
+});
